2d0b1ab523db7a6e0b39f57f6957e7b55da128fd
wuyuanyi <wuyuanyi@gmail.com>
2016-05-10 16:04:06 +0800

    add date selector

--- js/components/components/EntryForm.react.js
+++ js/components/components/EntryForm.react.js
@@ -1,7 +1,17 @@
 import {connect} from "react-redux";
 import React, {Component} from 'react';
-import {FormGroup, ControlLabel, FormControl, Panel, Glyphicon, InputGroup} from 'react-bootstrap';
+import {Button, ButtonGroup, FormGroup, ControlLabel, HelpBlock, FormControl, Panel, Glyphicon, InputGroup} from 'react-bootstrap';
 class EntryForm extends Component {
+    constructor() {
+        super();
+        this.state = {};
+
+        // Give selection a default value;
+        if (!this.state.dateSelected) {
+            this.state.dateSelected = 'dateRevised';
+        }
+    }
+
     _stateHelper (stateName, value) {
         var s = {}
         s[stateName] = value;
@@ -39,69 +49,117 @@ class EntryForm extends Component {
             </InputGroup>
         )
     }
+
+    selectDate(e) {
+        switch(e.target.id) {
+            case "btndateCreated":
+                this.setState({dateSelected: 'dateCreated'});
+                break;
+            case 'btndateCompleted':
+                this.setState({dateSelected: 'dateCompleted'});
+                break;
+            case 'btndateRevised':
+                this.setState({dateSelected: 'dateRevised'});
+                break;
+        };
+    }
+    getCurrentSelectedDate() {
+        var state = this.state;
+        switch (state.dateSelected) {
+            case 'dateCreated':
+                var dt = state.dateCreated;
+                if (Object.keys(dt).length)
+                    return <HelpBlock>当前：{dt.Year}-{dt.Month}-{dt.Day}</HelpBlock>
+                break;
+            case 'dateCompleted':
+                var dt = state.dateCompleted;
+                if (Object.keys(dt).length)
+                    return <HelpBlock>当前：{dt.Year}-{dt.Month}-{dt.Day}</HelpBlock>
+                break;
+            case 'dateRevised':
+                var dt = state.dateRevised;
+                if (Object.keys(dt).length)
+                    return <HelpBlock>当前：{dt.Year}-{dt.Month}-{dt.Day}</HelpBlock>
+                break;
+            default:
+                return null;
+        }
+    }
+
     componentWillReceiveProps(nextProps) {
         this.setState(nextProps.data);
     }
     componentWillMount() {
         this.setState(this.props.data);
     }
-    constructor() {
-        super();
-        this.state = {};
+
+    pmidPart() {
+        var state = this.state;
+        return(
+        <Panel header="PMID">
+            <FormGroup className="form-panel-content">
+                <ControlLabel>PMID</ControlLabel>
+                <FormControl
+                    type="text"
+                    placeholder="PMID"
+                    value={state.pmid}
+                    onChange={(e)=>this.setState({pmid:e.target.value})}
+                    ></FormControl>
+                {state.pmid ?
+                    (<p>
+                        前往链接 <Glyphicon glyph="globe"/> <a target="_blank" href={"http://www.ncbi.nlm.nih.gov/pubmed/?term=" + state.pmid}>
+                        {"http://www.ncbi.nlm.nih.gov/pubmed/?term=" + state.pmid }
+                        </a>
+                    </p>) : null}
+            </FormGroup>
+        </Panel>
+        );
+    }
+
+    datePart() {
+        var state = this.state;
+        return (
+        <Panel header="日期">
+            <FormGroup className="form-panel-content">
+                {}
+                <ControlLabel>创建日期</ControlLabel>
+                {this.dateInputGroup(state.dateCreated, (v)=>this.setState({dateCreated: v}))}
+                <ControlLabel>完成日期</ControlLabel>
+                {this.dateInputGroup(state.dateCompleted, (v)=>this.setState({dateCompleted: v}))}
+                <ControlLabel>修订日期</ControlLabel>
+                {this.dateInputGroup(state.dateRevised, (v)=>this.setState({dateRevised: v}))}
+            </FormGroup>
+            <FormGroup className="form-panel-content">
+                <ControlLabel>选择日期</ControlLabel>
+                <ButtonGroup block className="form-btn-group">
+                   <Button
+                       id='btndateCreated'
+                       active={state.dateSelected == 'dateCreated'}
+                       onClick={(e)=>this.selectDate(e)}
+                       >创建日期</Button>
+                   <Button
+                       id='btndateCompleted'
+                       active={state.dateSelected == 'dateCompleted'}
+                       onClick={(e)=>this.selectDate(e)}
+                       >完成日期</Button>
+                   <Button
+                       id='btndateRevised'
+                       active={state.dateSelected == 'dateRevised'}
+                       onClick={(e)=>this.selectDate(e)}
+                       >修订日期</Button>
+               </ButtonGroup>
+                {this.getCurrentSelectedDate()}
+            </FormGroup>
+
+        </Panel>
+        )
     }
     render() {
-        var state = this.state;
+
         return  (
             <form>
-                <Panel header="PMID">
-                    <FormGroup bsClass="form-panel-content">
-                        <ControlLabel>PMID</ControlLabel>
-                        <FormControl
-                            type="text"
-                            placeholder="PMID"
-                            value={state.pmid}
-                            onChange={(e)=>this.setState({pmid:e.target.value})}
-                            ></FormControl>
-                        {state.pmid ?
-                            (<p>
-                                前往链接 <Glyphicon glyph="globe"/> <a target="_blank" href={"http://www.ncbi.nlm.nih.gov/pubmed/?term=" + state.pmid}>
-                                {"http://www.ncbi.nlm.nih.gov/pubmed/?term=" + state.pmid }
-                                </a>
-                            </p>) : null}
-                    </FormGroup>
-                </Panel>
-
-                <Panel header="日期">
-                    <FormGroup bsClass="form-panel-content">
-                        <ControlLabel>创建日期</ControlLabel>
-                        {this.dateInputGroup(state.dateCreated, (v)=>this.setState({dateCreated: v}))}
-                        <ControlLabel>完成日期</ControlLabel>
-                        {this.dateInputGroup(state.dateCompleted, (v)=>this.setState({dateCompleted: v}))}
-                        <ControlLabel>修订日期</ControlLabel>
-                        {this.dateInputGroup(state.dateRevised, (v)=>this.setState({dateRevised: v}))}
-                    </FormGroup>
-                </Panel>
-                <Panel header="日期">
-                    <FormGroup bsClass="form-panel-content">
-                        <ControlLabel>创建日期</ControlLabel>
-                        {this.dateInputGroup(state.dateCreated, (v)=>this.setState({dateCreated: v}))}
-                        <ControlLabel>完成日期</ControlLabel>
-                        {this.dateInputGroup(state.dateCompleted, (v)=>this.setState({dateCompleted: v}))}
-                        <ControlLabel>修订日期</ControlLabel>
-                        {this.dateInputGroup(state.dateRevised, (v)=>this.setState({dateRevised: v}))}
-                    </FormGroup>
-                </Panel>
-                <Panel header="日期">
-                    <FormGroup bsClass="form-panel-content">
-                        <ControlLabel>创建日期</ControlLabel>
-                        {this.dateInputGroup(state.dateCreated, (v)=>this.setState({dateCreated: v}))}
-                        <ControlLabel>完成日期</ControlLabel>
-                        {this.dateInputGroup(state.dateCompleted, (v)=>this.setState({dateCompleted: v}))}
-                        <ControlLabel>修订日期</ControlLabel>
-                        {this.dateInputGroup(state.dateRevised, (v)=>this.setState({dateRevised: v}))}
-                    </FormGroup>
-                </Panel>
-
+                {this.pmidPart()}
+                {this.datePart()}
             </form>
         );
     }
